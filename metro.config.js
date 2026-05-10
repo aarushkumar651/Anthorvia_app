afef29d1bdd4280c7802adbaa8b728b189e47dd7
AARUSH KUMAR <aarushkumar.651@gmail.com>
2026-05-10 13:49:58 +0530

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: [...defaultConfig.resolver.assetExts, 'sksl'],
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'mjs'],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
